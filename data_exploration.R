bf263f1027119c538010cfc6833720d3c8f4231b
tianxiaoyan2024 <161792631+tianxiaoyan2024@users.noreply.github.com>
2024-04-21 17:58:00 +0800

    Update data_exploration.R

--- data_exploration.R
+++ data_exploration.R
@@ -1,146 +1,146 @@
-## ---------------------------
-##
-## Script name: data_exploration.R
-##
-## Purpose of script: 
-## 1. write a short code to remove the sites with missing data of the Doubs dataset,
-## and detect whether environmental factors are collinearity.
-## 2. Analysis on the relationships between fishes and environment factors and 
-## visualize such relationships.
-##
-## Author: Weishan Tu
-##
-## Date Created: 2024-04-14
-##
-## Copyright (c) Timothy Farewell, 2024
-## Email: weishan@mail.ustc.edu.cn
-##
-## ---------------------------
+# --------------------------------------------
+# Script Name: data_exploration.R
+# Purpose1: This scribes how to remove the sites with missing data of the  Doubs
+#           dataset, and detect whether environmental factors are collinearity.
+# Purpose2: Analysis on the relationships between fishes and environment
+#           factors and visualize such relationships.
 
-cat("\014") #clears rhe console
-rm(list=ls()) #remove all variales
 
-# install.packages("ade4")
-##Load package 
+# Author:     Xiaoyan Tian
+# Email:      xiaoyantian@mail.ustc.edu.cn
+# Date:       2024-04-15
+
+# --------------------------------------------
+# Load the package
 library(ade4)
 library(vegan)
-library(tidyverse)
+library(ape)
+library(dplyr)
+# Load the required dataset
+data("doubs")
+doubs
+str(doubs)
+str(env)
 
-##Load data
-data(doubs)
-
-spe <- doubs$fish
+fish <- doubs$fish
 env <- doubs$env
 spa <- doubs$xy
 
-spe <- spe[-8,]
-env <- env[-8,]
-spa <- spa[-8,]
-knitr::kable(spe[1:3,1:5])
-str(doubs.spe)
-head(doubs)
-decorana(doubs.env)
+# --------------------------------------------
+# remove the sites with missing data 
+# --------------------------------------------
+# 1.Check for missing values in the entire data frame
+anyNA(doubs)
 
-doubs.spe <- doubs$species #Species list 27 species 
-doubs.env <- doubs$env #11 abiotic variables
-str(doubs.env)
-doubs.xy <- doubs$xy #Spatial coordinates
-doubs.fish <- doubs$fish # Community data
+any_missing_in_data <- any(is.na(doubs))
+cat("Does the data frame have missing data?", any_missing_in_data)
+#No NA in the "doubs"
+#If the data has misssing value, we can filter it by the following
+doubs_no_na <- doubs %>%
+  filter_all(all_vars(!is.na(.)))
+#OR another method to filter
+doubs_clean <- na.omit(doubs)
 
-#
-sit.pres <- apply(doubs.spe > 0, 1, sum)
+# 2.Know the species richness of each site
+# 2.1 Check the sturcture for data frame of species firstly
+colSums(doubs.fish)
+summary(doubs.fish)
+rowSums(fish) #Note:there are no species in site 8 
+
+# 2.2 Next Illustrate the species richness of each site along the river
+
+sit.pres <- apply(fish > 0, 1, sum)
 sort(sit.pres)
 par(mfrow=c(1,2))
 plot(sit.pres,type="s", las=1, col="gray",
      main="Species Richness vs. \n Upstream-Downstream Gradient",
      xlab="Positions of sites along the river", ylab="Species richness")
 text(sit.pres, row.names(doubs.spe), cex=.8, col="red")
-plot(coordinates_utm, asp=1, main="Map of Species Richness", pch=21, col="white",
+plot(spa, asp=1, main="Map of Species Richness", pch=21, col="white",
      bg="brown", cex=5*sit.pres/max(sit.pres), xlab="x coordinate (km)",
      ylab="y coordinate (km)")
-lines(coordinates_utm, col="light blue")
+lines(spa, col="light blue")
+#Only the No.8 sample has no fish along the river
 
+# 3.remove the No.8 site to clean the missing data 
+fish <- fish[-8,]
+env <- env[-8,]
+spa <- spa[-8,]
+View(fish)
+knitr::kable(spe[1:9,1:5])
 
-##
-##Remove the sites with missing data
-str(doubs.fish)  # structure of objects in dataset
-rowSums(doubs.fish)  # row 8 no species
-rowSums(doubs.spe)
-colSums(doubs.fish)
-anyNA(doubs.fish) # No NA
-summary(doubs.fish)  # summary statistics for all objects (min, mean, max, etc.)
-anyNA(doubs.env) # No NA
+# --------------------------------------------
+# Delete the collinear environmental factors by R mode
+# --------------------------------------------
+#R-mode (analysis of relationships among variables)
+# Pearson r linear correlation
+env.pearson<-cor(env) 
+round(env.pearson, 2) #Rounds the coefficients to 2 decimal points 
 
-range(doubs.fish)
-ab <- table(unlist(doub))
-str(doubs.)
-barplot(ab,las=1,xlab = "abund degree",ylab="frequency",col=grey(5:0/5))
-sum(doubs.spe==0)
-levels(doubs.spe$code)
+# Kendall tau rank correlation
+env.kendall<-cor(env, method="kendall") 
+round(env.kendall, 2)
 
-code_columns <- paste0("code", 1:27)
-code_vector <- unlist(doubs[, code_columns])
+#Method2
+library(stats)
+op <- par(mfrow=c(1,1), pty="s")
+pairs(doubs.env, panel=panel.smooth,
+      diag.panel=panel.hist,
+      main="Biplots with histograms and smooth surves")
+par(op)
 
-sum(doubs.fish==0)
+# Method3
+# Reference:https://blog.csdn.net/weixin_39886469/article/details/111017010  
+#           http://sthda.com/english/wiki/correlation-matrix-a-quick-start-guide-to-analyze-format-and-visualize-a-correlation-matrix-using-r-software
+library(Hmisc)
+corr_matrix <- rcorr(as.matrix(env))
+corr_matrix
 
-sum(spe==0)/(nrow(spe)*ncol(spe))
-sum(doubs.fish==0)/(nrow(doubs.fish)*ncol(doubs.fish))
+print(class(corr_matrix))
+print(corr_matrix)
 
-doubs.fish <- doubs.fish[-8, ]  # Site number 8 contains no species, so we remove row 8 (site 8) 
-doubs.env <- doubs.env[-8, ]  # Remove corresponding abiotic data for site 8 (because removed from fish data). 
-doubs.xy <- doubs.xy[-8, ] # Remove xy data for site 8 (because removed from fish data). 
+library(corrplot)
+corrplot(corr_matrix$r, type = "upper", order = "hclust", tl.col = "black", tl.srt = 45)
 
+corr <- cor(env) # 计算相关系数矩阵
+corrplot(corr, method = "color") # 使用颜色绘制相关系数图
 
-##########################
-##1 Coinertia Analysis
-##########################
-# Calculate the correlation matrix
-corr_matrix <- cor(doubs.env)
+install.packages("PerformanceAnalytics", dependencies = TRUE)
+library(PerformanceAnalytics)
+chart.Correlation(env, histogram=T, pch=19)
+#Note: there is a high collinearity between dfs and other environmental factors
+# --------------------------------------------
+# Analysis on the relationships between fishes and environment
+# --------------------------------------------
+# Selection of unconstrained ordination base on CDA analysis
+dca <- decorana(fish)
+print(dca)
+# 3.0 < DCA1==3.855 < 4.0, RDA and CCA are both OK
 
-# Calculate the correlation matrix p-value
-library(Hmisc)#load package
-corr_matrix2 <- rcorr(as.matrix(doubs.env))
-corr_matrix2
+# Method1: Using RDA
+# 1.Standardize and transform the data
+# 1.1 The Hellinger transformation of the coummunity species to correct for the double zero problem
+fish.hel <- decostand(fish, "hellinger")
+# Calculate distance matrix
+fish.bchel<-vegdist(fish.hel, method="bray", binary=FALSE) 
 
-# Visualization of a Correlation Matrix
-library(corrplot)#load package
-corrplot(corr_matrix, type = "upper", order = "hclust", tl.col = "black", tl.srt = 45)
+# 1.2 The Z-score standardization of the environmental factor for normal distribution
+env.z <- decostand(env, "standardize")
+apply(env.z, 2, mean) # means = 0
+apply(env.z, 2, sd) # standard deviations = 1
+# OR same standardization with scale() 
+env.z1 <- as.data.frame(scale(env))
 
-#dts is highly correlated with alt, slo, flo, har,nit...
-# Draw scatter plots
-# pak::pak("PerformanceAnalytics")
-library(PerformanceAnalytics)#load package
-chart.Correlation(doubs.env, histogram=TRUE, pch=19)
+# 1.3 Remove the environmental factor of the 'dfs',showing collinearity with other factors
+env.z2 <- subset(env.z, select = -which(names(env.z) == "dfs"))
 
-##########################
-##2 The relationships between fishes and environment factors
-##########################
-# Check unconstrained and constrained
-vare.dca <- decorana(doubs.env)
-# Axis lengths=1.75 <3 
-# Use constrained ordination 
-# Use RDA analysis
-
-# Step 1: Transform and standardize the data.
-# Hellinger transform the community data
-?decostand
-#
-doubs.fish.hel <- decostand(doubs.fish, method = "hellinger")
-
-#Scale and center env variables
-doubs.env.z <- decostand(doubs.env, method = "standardize")
-# Variables are now centered around a mean of 0
-round(apply(doubs.env.z, 2, mean), 1)
-# and scaled to have a standard deviation of 1
-apply(doubs.env.z, 2, sd) 
-
-# use standardized environmental data
-# remove 'dfs', which was correlated with many other variables
-doubs.env.z <- subset(doubs.env.z, select = -dfs)
-
-# Step 2: Select environmental variables.
-# Initial RDA with ALL of the environmental data
-doubs.fish.rda <- rda(doubs.fish.hel ~ ., data = doubs.env.z)
+# 2. Run the RDA and select environmental variables
+dim(fish.hel)
+dim(env.z2)
+rda_fish <- rda(fish.hel ~ ., data = env.z2)
+summary(rda_fish)
+rda_fish$call
+#rda(formula = fish.hel ~ alt + slo + flo + pH + har + pho + nit + amm + oxy + bdo, data = env.z2)
 
 # Forward selection of environmental variables
 fwd.sel <- ordiR2step(rda(doubs.fish.hel ~ 1, data = doubs.env.z),
@@ -149,23 +149,52 @@ fwd.sel <- ordiR2step(rda(doubs.fish.hel ~ 1, data = doubs.env.z),
 fwd.sel$call
 # rda(formula = doubs.fish.hel ~ alt + oxy + bdo, data = doubs.env.z)
 
-# Step 3: Run the RDA and check its explanatory power.
+# Run the new model between species and the selected environmental factors
+rda_signif_fish <- rda(fish.hel ~ alt + oxy + bdo, data = env.z2)
+# !变量待修改Check R^2 retreived from the rda result
+R2 <- RsquareAdj(spe.rda)$r.squared # unadjusted R^2 
+R2 
+R2adj <- RsquareAdj(spe.rda)$adj.r.squared # adjusted R^2
+R2adj 
 
-# Re-run the RDA with the significant variables
-doubs.fish.rda.signif <- rda(formula = doubs.fish.hel ~ alt + oxy + bdo, data = doubs.env.z)
-
-# Find the adjusted R2 of the model with the retained env
-# variables
-RsquareAdj(doubs.fish.rda.signif)$adj.r.squared
-##0.5401552
-
-# Step 4: Test model significance.
-anova.cca(doubs.fish.rda.signif, step = 1000)
+# Check and test model significance.
+anova.cca(rda_signif_fish, step = 1000)
 
 # selected environmental variables significantly explain 54.0% (p = 0.001)
 
-# Step 5: Plot the RDA results
+# Plot the RDA results
+# plot RDA
 # Scaling 1
 ordiplot(doubs.fish.rda.signif, scaling = 1, main = "doubs RDA - Scaling 1")
 # Scaling 2
 ordiplot(doubs.fish.rda.signif, scaling = 2, main = "doubs RDA - Scaling 2")
+
+
+# Triplot: sites, response variables and explanatory variables
+# Scaling 1
+plot(spe.rda, scaling=1, main="scaling 1 - wa scores")
+spe.sc <- scores(spe.rda, choices=1:2, scaling=1, display="sp")
+arrows(0,0,spe.sc[,1], spe.sc[,2], length=0, lty=1, col='red')
+
+# Scaling 2
+plot(spe.rda, main="scaling 2 - wa scores")
+spe2.sc <- scores(spe.rda, choices=1:2, display="sp")  
+arrows(0,0,spe2.sc[,1], spe2.sc[,2], length=0, lty=1, col='red')
+
+
+
+
+# 假设env.z2是你的环境数据矩阵
+pca_results <- prcomp(env.z2, scale = TRUE) # 对环境数据进行PCA分析
+
+# 查看PCA结果
+summary(pca_results)
+
+# 绘制PCA结果的散点图或双标图来可视化变量之间的关系
+biplot(pca_results, scale = 0, cex = 0.6)
+
+# 你可以使用PCA的主成分作为新的预测变量进行RDA分析
+# 这里假设你保留了前几个主成分，这些主成分解释了环境数据的大部分变异
+rda_results <- rda(fish.hel ~ PC1 + PC2, data = as.data.frame(pca_results$x))
+
+
